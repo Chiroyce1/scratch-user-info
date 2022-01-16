88e0bbab377c5146fbcec3bf498bbffa97b5887a
Chiroyce <97374054+Chiroyce1@users.noreply.github.com>
2022-01-16 11:14:44 +0530

    Add error checking for when a service is down

--- main.py
+++ main.py
@@ -22,28 +22,33 @@ def banner():
 {Fore.GREEN}=========================================================
     """)
 
+
 banner()
 
 print(f"{Fore.CYAN}[+] Checking dependencie(s) . . .")
 try:
     import requests
+    import requests
 except ModuleNotFoundError:
     sys.exit(f"\n{Fore.RED}Requests module not found\n\nPlease install https://pypi.org/project/requests/ for this to work.\n")
-print(f"{Fore.CYAN}[+] Dependencie(s) are up to date . . . {Fore.GREEN}requests>=2.27.1\n")
+print(
+    f"{Fore.CYAN}[+] Dependencie(s) are up to date . . . {Fore.GREEN}requests>=2.27.1\n")
 
-version = requests.get('https://raw.githubusercontent.com/Chiroyce1/scratch-user-data/main/version.txt').text
+version = requests.get(
+    'https://raw.githubusercontent.com/Chiroyce1/scratch-user-data/main/version.txt').text
 
 if float(VERSION) < float(version):
     print("Update available for Scratch User Data.\nYou can continue to use this version,\nbut updating is recommended.")
 
-if  len(sys.argv) > 1:
+if len(sys.argv) > 1:
     username = sys.argv[1]
     print(f"{Fore.CYAN}[+] Input taken from sys args: {Fore.GREEN}{username}")
 else:
     username = input(f"\n{Fore.CYAN}Enter USERNAME\n>>> {Fore.GREEN}")
 
 print(f"\n{Fore.CYAN}[+] Validating username . . . (1/3)")
-response = requests.get(f'https://api.scratch.mit.edu/users/{username}/projects').json()
+response = requests.get(
+    f'https://api.scratch.mit.edu/users/{username}/projects').json()
 print(f"\n{Fore.CYAN}[+] Checking for valid shared projects . . (2/3)\n")
 try:
     projectID = response[0]['id']
@@ -56,40 +61,56 @@ except KeyError:
     sys.exit(f"{Fore.RED}{username} is an invalid username.\n")
 
 if hasProjects:
-    response = requests.get(f'https://projects.scratch.mit.edu/{projectID}/').json()
+    response = requests.get(
+        f'https://projects.scratch.mit.edu/{projectID}/').json()
     userAgent = response['meta']['agent']
 print("[+] Getting user data . . . (3/3)\n")
-response = requests.get(f'https://api.scratch.mit.edu/users/{username}/').json()
-postData = requests.get(f'https://scratchdb.lefty.one/v3/forum/user/info/{username}').json()
-ocularData = requests.get(f'https://my-ocular.jeffalo.net/api/user/{username}').json()
+try:
+    response = requests.get(
+        f'https://api.scratch.mit.edu/users/{username}/').json()
+    postData = requests.get(
+        f'https://scratchdb.lefty.one/v3/forum/user/info/{username}').json()
+    ocularData = requests.get(
+        f'https://my-ocular.jeffalo.net/api/user/{username}').json()
+except Exception as e:
+    print(f"{Fore.RED}Could not get some details due to an error...\n")
 print("===============================================\n")
 print(f"{Fore.CYAN}Username                 - {Fore.GREEN}{username}\n")
 print(f"{Fore.CYAN}User ID                  - {Fore.GREEN}{response['id']}\n")
-print(f"{Fore.CYAN}Joined date              - {Fore.GREEN}{response['history']['joined']}\n")
-print(f"{Fore.CYAN}Country                  - {Fore.GREEN}{response['profile']['country']}\n")
-print(f"{Fore.CYAN}Scratch Team Member?     - {Fore.GREEN}{response['scratchteam']}\n")
+print(
+    f"{Fore.CYAN}Joined date              - {Fore.GREEN}{response['history']['joined']}\n")
+print(
+    f"{Fore.CYAN}Country                  - {Fore.GREEN}{response['profile']['country']}\n")
+print(
+    f"{Fore.CYAN}Scratch Team Member?     - {Fore.GREEN}{response['scratchteam']}\n")
 if hasProjects != True:
     print(details)
 try:
-    print(f"{Fore.CYAN}Total Forum Posts        - {Fore.GREEN}{postData['counts']['total']['count']}\n")
+    print(
+        f"{Fore.CYAN}Total Forum Posts        - {Fore.GREEN}{postData['counts']['total']['count']}\n")
 except KeyError:
     print(f"{Fore.CYAN}Total Forum posts   - {Fore.GREEN}0")
 try:
-    print(f"{Fore.CYAN}Forum Leaderboard Rank   - {Fore.GREEN}{postData['counts']['total']['rank']}\n")
+    print(
+        f"{Fore.CYAN}Forum Leaderboard Rank   - {Fore.GREEN}{postData['counts']['total']['rank']}\n")
 except KeyError:
     print(f"{Fore.CYAN}Forum Leaderboard Rank    - {Fore.GREEN}NA | {Fore.RED} User has no posts")
 if hasProjects != False:
     print(f"{Fore.CYAN}User-Agent - \n{Fore.GREEN}{userAgent}\n")
     print(f"{Fore.CYAN}User-Agent lastUpdate    - {Fore.GREEN}{date}\n")
 try:
-    print(f"{Fore.CYAN}Ocular Status -            \n{Fore.GREEN}{ocularData['status']}\n")
+    print(
+        f"{Fore.CYAN}Ocular Status -            \n{Fore.GREEN}{ocularData['status']}\n")
 except KeyError:
     print(f"{Fore.CYAN}Ocular Status -            \n{Fore.GREEN}NA | {Fore.RED}User has no Ocular Status\n")
 try:
-    print(f"{Fore.CYAN}Ocular lastUpdate        -           {Fore.GREEN}{ocularData['meta']['updated']}\n")
+    print(
+        f"{Fore.CYAN}Ocular lastUpdate        -           {Fore.GREEN}{ocularData['meta']['updated']}\n")
 except KeyError:
     print(f"{Fore.CYAN}Ocular lastUpdate -            \n{Fore.GREEN}NA | {Fore.RED}User has no Ocular Status\n")
-print(f"{Fore.CYAN}About Me -            \n{Fore.GREEN}{response['profile']['bio']}\n")
-print(f"{Fore.CYAN}What I'm working on - \n{Fore.GREEN}{response['profile']['status']}\n")
+print(
+    f"{Fore.CYAN}About Me -            \n{Fore.GREEN}{response['profile']['bio']}\n")
+print(
+    f"{Fore.CYAN}What I'm working on - \n{Fore.GREEN}{response['profile']['status']}\n")
 print('\n')
 input(f"{Fore.CYAN}Press enter to exit\n")
